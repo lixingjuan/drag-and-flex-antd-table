8030e6c06ef617757c8f23de6b4fe59f08b8a65b
lixingjuan <xingjuan.li@datayes.com>
2020-12-26 16:17:03 +0800

    add: 添加发布分支

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+// import Page from './pages';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Page />
+//     </div>
+//   );
+// }
+
+// export default App;
 import './App.css';
+import DragAndFlexTable from './drag-and-flex-table';
 
-import Page from './pages';
-
-function App() {
-  return (
-    <div className="App">
-      <Page />
-    </div>
-  );
-}
-
-export default App;
+export default DragAndFlexTable;
